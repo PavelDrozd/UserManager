9c7d848eb2a465c49bcc24be5d08f21cb5e95eea
PavelDrozd <87943574+PavelDrozd@users.noreply.github.com>
2023-04-11 18:16:24 +0300

    Delete extra tables

--- docker/mysql/schema.sql
+++ docker/mysql/schema.sql
@@ -1,12 +1,9 @@
 /*
 drop table users;
 drop table images;
-drop table status;
 */
 
 -- create database usermanager;
-
-create table status(id int not null auto_increment primary key, name varchar(20) unique not null);
 
 create table images(id bigint not null auto_increment primary key, name varchar(100), file_name varchar(250), size bigint, content_type varchar(10), is_preview_image boolean, bytes longblob, user_id bigint);
 
